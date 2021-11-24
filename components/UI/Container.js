5a560076c7e1f2cd056bba69e549ae66a8a16f65
Alassane Ndoye <aloutndoye.an@gmail.com>
2021-11-24 15:31:25 +0000

    install heroicons

--- components/UI/Container.js
+++ components/UI/Container.js
@@ -1,3 +1,5 @@
 export default function Container(props) {
-  return <div className="xl:container mx-auto px-16">{props.children}</div>;
+  return (
+    <div className="md:container md:mx-auto md:px-16">{props.children}</div>
+  );
 }
